9d146035e36db227975fbb6c72f1379c44a920e6
root <root@localhost.localdomain>
2017-02-08 00:09:22 +0530

    git

---
+++ Lab2_1_2.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+
+public class Lab2_1_2
+{
+  public static void main( String args[] )
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:student.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM STUDENT;" );
+      while ( rs.next() ) {
+         int id = rs.getInt("SAP_ID");
+         String  name = rs.getString("NAME");
+         int age  = rs.getInt("AGE");
+         String  address = rs.getString("ADDRESS");
+         float class = rs.getFloat("CLASS");
+         System.out.println( "ID = " + id );
+         System.out.println( "NAME = " + name );
+         System.out.println( "AGE = " + age );
+         System.out.println( "ADDRESS = " + address );
+         System.out.println( "SALARY = " + class );
+         System.out.println();
+      }
+
+
+      rs.close();
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Table created successfully");
+  }
+}
